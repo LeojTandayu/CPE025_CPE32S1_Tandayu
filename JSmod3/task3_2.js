b3d10d71f972a2e3d461be022b031ca885600db7
LeojTandayu <qljbtandayu@tip.edu.ph>
2026-02-27 15:14:00 +0800

    02-27-26

---
+++ JSmod3/task3_2.js
@@ -0,0 +1,6 @@
+console.log("3.2.4 SECTION PRACTICE");
+let width = Number(prompt("Volume of the box, enter width", 0));
+let height = Number(prompt("Volume of the box, enter height", 0));
+let length = Number(prompt("Volume of the box, enter length", 0));
+let volume = width * height * length;
+alert(`Calculated box volume is ${volume}`);
